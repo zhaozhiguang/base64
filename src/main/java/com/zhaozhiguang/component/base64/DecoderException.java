d9ea9bc23cca307a1d0ffffe8bd92767e894f842
Administrator <1422615221@qq.com>
2018-05-18 10:30:25 +0800

    base64工具类

---
+++ src/main/java/com/zhaozhiguang/component/base64/DecoderException.java
@@ -0,0 +1,24 @@
+package com.zhaozhiguang.component.base64;
+
+/**
+ * 解码异常
+ * @author zhaozhiguang
+ */
+public class DecoderException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public DecoderException() {
+    }
+
+    public DecoderException(String message) {
+        super(message);
+    }
+
+    public DecoderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DecoderException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/zhaozhiguang/component/base64/EncoderException.java
@@ -0,0 +1,24 @@
+package com.zhaozhiguang.component.base64;
+
+/**
+ * 编码异常
+ * @author zhaozhiguang
+ */
+public class EncoderException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public EncoderException() {
+    }
+
+    public EncoderException(String message) {
+        super(message);
+    }
+
+    public EncoderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EncoderException(Throwable cause) {
+        super(cause);
+    }
+}
